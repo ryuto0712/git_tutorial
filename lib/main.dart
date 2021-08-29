3fd0f3212c764a385bbd379cb32bd8a8d3613490
ryuto0712 <oginoryuto@icloud.com>
2021-08-30 00:39:06 +0900

    second

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Pageaaaaaaa'),
+      home: MyHomePage(title: 'Flutter Demo aaeaaaaaaa'),
     );
   }
 }
